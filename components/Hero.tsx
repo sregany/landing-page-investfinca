585a2f3a6f0d759be7172ec955f14729c49b2832
sregany <sergiregany1996@gmail.com>
2026-03-10 18:16:26 +0100

    style: remove scroll indicator from hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -67,16 +67,7 @@ const Hero = () => {
         </motion.div>
       </div>
 
-      {/* Scroll Indicator */}
-      <motion.div 
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 1.5, duration: 1 }}
-        className="absolute bottom-10 left-1/2 -translate-x-1/2 flex flex-col items-center gap-2"
-      >
-        <div className="w-[1px] h-12 bg-gradient-to-b from-white to-transparent" />
-        <span className="text-[10px] text-white/60 tracking-widest uppercase">Scroll</span>
-      </motion.div>
+
     </section>
   );
 };
